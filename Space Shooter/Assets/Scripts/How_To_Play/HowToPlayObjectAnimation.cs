2fb41bf491def56f861bdcc352acaaaa33e30064
KingZoldack <50232232+KingZoldack@users.noreply.github.com>
2021-08-10 02:10:09 +0100

    How To Play Warp
    Completed warp animation.

--- Space Shooter/Assets/Scripts/How_To_Play/HowToPlayObjectAnimation.cs
+++ Space Shooter/Assets/Scripts/How_To_Play/HowToPlayObjectAnimation.cs
@@ -6,7 +6,7 @@ public class HowToPlayObjectAnimation : MonoBehaviour
 {
     [SerializeField]
     float _astroidRotationSpeed = 2.5f;
-    float _playerMoveSpeed = 0.5f;
+    float _playerMoveSpeed = 1.5f;
 
     bool startMovement = false;
 
@@ -21,21 +21,29 @@ public class HowToPlayObjectAnimation : MonoBehaviour
     {
         RotateAstroid();
 
-        //if (this.gameObject.tag == "How_To_Play_Player")
-        //{
- 
-        //        transform.Translate(Vector3.right * _playerMoveSpeed * Time.deltaTime);
+        if (this.gameObject.tag == "Warp_Text_Player")
+        {
 
+           transform.Translate(Vector3.up * _playerMoveSpeed * Time.deltaTime);
+        }
 
-        //    if (transform.position.x >= -2.86f)
+        //    if (transform.position.x >= 10.50f)
         //    {
-        //        _playerMoveSpeed = -0.5f;
+        //        Vector3 warpToRight = new Vector3(-10.03f, transform.position.y, transform.position.z);
+        //        transform.position = warpToRight;
         //    }
 
-        //    else if (transform.position.x <= -5.13f)
-        //    {
-        //        _playerMoveSpeed = 0.5f;
-        //    }
+        //    //if (transform.position.x == -7.93)
+        //    //{
+        //    //    gameObject.SetActive(false);
+        //    //}
+
+
+
+        //    //else if (transform.position.x <= -5.13f)
+        //    //{
+        //    //    _playerMoveSpeed = 0.5f;
+        //    //}
         //}
 
 
@@ -48,6 +56,36 @@ public class HowToPlayObjectAnimation : MonoBehaviour
             transform.Rotate(Vector3.forward * _astroidRotationSpeed * Time.deltaTime);
         }
     }
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Teleport_To_Right")
+        {
+            Vector3 warpToRight = new Vector3(-10.03f, transform.position.y, transform.position.z);
+            transform.position = warpToRight;
+        }
+
+        else if (other.tag == "Turn_Back_On_Left_Side")
+        {
+            Vector3 faceLeft = new Vector3(0f, 0f, 90f);
+            //this.gameObject.SetActive(false);
+            transform.rotation = Quaternion.Euler(faceLeft);
+        }
+
+        else if (other.tag == "Teleport_To_Left")
+        {
+            Vector3 warpToLeft = new Vector3(10.23f, transform.position.y, transform.position.z);
+            transform.position = warpToLeft;
+        }
+
+        else if (other.tag == "Turn_Back_On_Right_Side")
+        {
+            Vector3 faceRight = new Vector3(0f, 0f, -90f);
+            //this.gameObject.SetActive(false);
+            transform.rotation = Quaternion.Euler(faceRight);
+        }
+    }
+
 
     //IEnumerator MovingPlayer()
     //{
